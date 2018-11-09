ac8cedcd40c5bbb8c1fae6c845e4966ac5f093b5
vgeorgakopoylos <v.georgakopoylos@gmai.com>
2018-11-09 16:28:25 +0200

    modified:   conf/configuration.cfg modified:   httpServer.py modified:   themoviedb.py modified:   themoviedb.pyc

--- httpServer.py
+++ httpServer.py
@@ -2,13 +2,22 @@ import SimpleHTTPServer
 import SocketServer
 import themoviedb
 import os
+import configparser
 
+def initialization():
+	global defaultPage;
+	global mainFile;
+
+	config = configparser.ConfigParser();
+	relDir = os.path.dirname(os.path.abspath(__file__))
+	config.read(relDir+'\conf\configuration.cfg');		
+	defaultPage = config.get('System','defaultPage');
+	mainFile = config.get('System','mainFile');
 
 def breakdownParams(link):
 	returnedParams = '';
 	questionMarkPos = link.find('?');
 	if questionMarkPos>0:
-		print '**********:'+link[questionMarkPos+1:].replace("&", ",")
 		return link[questionMarkPos+1:].replace("&", ",");	
 	else:
 		return '';
@@ -22,11 +31,18 @@ def findWrapper(var,charToFind):
 	if var.find('?') == -1:
 		return len(var);
 	return var.find(charToFind); 
+	
+def decideURL(requestURL):
+	if requestURL == "/":
+		return mainFile  + defaultPage +'()';
+	else:
+		return mainFile +requestURL[1:findWrapper(requestURL,'?')]+'('+breakdownParams(requestURL)+')'	
   
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
-		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self.wfile.write(eval("themoviedb."+self.path[1:findWrapper(self.path,'?')]+'('+breakdownParams(self.path)+')')));
+		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self.wfile.write(eval(decideURL(self.path))));
 
+initialization()
 Handler = MyRequestHandler;
 server = SocketServer.TCPServer(('0.0.0.0', 80), Handler);
 server.serve_forever();	
--- themoviedb.py
+++ themoviedb.py
@@ -2,9 +2,8 @@ import json
 import configparser
 import os
 import requests
-import json
-import SimpleHTTPServer
-import SocketServer
+import logging
+import math
 
 #function definitions start
 def initialization():
@@ -16,9 +15,11 @@ def initialization():
 	global language;
 	global imageURL;
 	global imageWidth;
+	
 	config = configparser.ConfigParser();
-	relDir = os.path.dirname(__file__);
+	relDir = os.path.dirname(os.path.abspath(__file__))
 	config.read(relDir+'\conf\configuration.cfg');	
+	logging.basicConfig(filename= relDir + '\\logs\\app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s');
 	version = config.get('General','version3');
 	mainURL = config.get('General','mainurl');
 	apiKey = config.get('General','apikeyV3');
@@ -30,21 +31,47 @@ def urlBuilder(methdName, paramerers):
 	if config.get('Method Types',methdName) == 'General':
 		url = mainURL + version + config.get('Method Suffix',methdName)+config.get('Methods',methdName)+'?'+'api_key='+apiKey+'&language='+config.get('General','language')+paramerers;
 		return url;
+
 def apiCalls(url):
 	response = requests.get(url);
 	if (response.status_code!=200):
 		raise Exception("API Call was not completed correctly. Status Code returned:"+str(response.status_code));
 	return response;
 
+def producePagingHTML(totalViewPages, pageTag, placeholder, upcomingMoviesPages, moviePage, showPrevNext):	
+	currentPage = 1;
+	htmlPage = '';
+	
+	if moviePage != 1 and showPrevNext==True:
+		htmlPage = htmlPage + pageTag.replace(placeholder,str(moviePage-1),1).replace(placeholder,'Previous');
+			
+	for i in range(1,totalViewPages+1):
+		if totalViewPages >= upcomingMoviesPages:
+			currentPage = i;
+		else:
+			if moviePage+(totalViewPages/2)<=upcomingMoviesPages and moviePage-(math.floor(totalViewPages/2))>0:
+				currentPage = moviePage-(totalViewPages/2)+i;
+			elif moviePage+(totalViewPages/2)>upcomingMoviesPages and moviePage-(math.floor(totalViewPages/2))>0:
+				currentPage = moviePage -(totalViewPages-(upcomingMoviesPages-moviePage))+i;
+			elif moviePage+(totalViewPages/2)<=upcomingMoviesPages and moviePage-(math.floor(totalViewPages/2))<=0:
+				currentPage = i;				
+			
+		htmlPage = htmlPage + pageTag.replace(placeholder,str(currentPage));
+		if currentPage==upcomingMoviesPages:
+			break;
+	if moviePage != upcomingMoviesPages:
+		htmlPage = htmlPage + pageTag.replace(placeholder,str(moviePage+1),1).replace(placeholder,'&nbsp&nbspNext');	
+	
+	return htmlPage;
 #function definitions end
-def upcomingMovies(moviepage=1):
+def upcomingMovies(moviePage=1):
 	initialization();
 	
 	html = '<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="mystyle.css"></head><body><div class="centered"><table>'
 		
 	rowChange = 1;
 
-	upcomingURL = urlBuilder('upcoming','&page='+str(moviepage));
+	upcomingURL = urlBuilder('upcoming','&page='+str(moviePage));
 	response = apiCalls(upcomingURL);
 	upcomingMoviesOverall = json.loads(response.text);
 	upcomingMoviesPages = upcomingMoviesOverall['total_pages'];
@@ -64,27 +91,8 @@ def upcomingMovies(moviepage=1):
 	
 	html = html + '<tr align="center"><td>';
 	
+	html = html + producePagingHTML(10, '<a href=\"upcomingMovies?moviePage=@#\">@#</a>  ', '@#', upcomingMoviesPages, moviePage, True);
 	
-	for i in range(1,11):
-		if i<=5:
-			if upcomingMoviesPages-moviepage<=10 and moviepage-2<=0:
-				html = html + '<a href=\"upcomingMovies?moviepage='+str(i)+'\">'+str(i)+'</a>  ';
-			if upcomingMoviesPages-moviepage<=10 and moviepage-2>0:
-				html = html + '<a href=\"upcomingMovies?moviepage='+str(moviepage-2+i)+'\">'+str(moviepage-2+i)+'</a>  ';				
-			if upcomingMoviesPages-moviepage>10 and moviepage-2<=0:
-				html = html + '<a href=\"upcomingMovies?moviepage='+str(i)+'\">'+str(i)+'</a>  ';
-			if upcomingMoviesPages-moviepage>10 and moviepage-2>0:
-				html = html + '<a href=\"upcomingMovies?moviepage='+str(moviepage-2+i)+'\">'+str(moviepage-2+i)+'</a>  ';								
-		if i==5 and (upcomingMoviesPages>8 or upcomingMoviesPages-moviepage>8):
-			html = html + '.... ';
-		if i>5:
-			if upcomingMoviesPages-moviepage<=10 or upcomingMoviesPages-10+i<=moviepage+i:
-				html = html + '<a href=\"upcomingMovies?moviepage='+str(i)+'\">'+str(i)+'</a>  ';
-			if upcomingMoviesPages-moviepage<10 and upcomingMoviesPages-10+i>moviepage+i:
-				html = html + '<a href=\"upcomingMovies?moviepage='+str(upcomingMoviesPages-10+i)+'\">'+str(upcomingMoviesPages-10+i)+'</a>  ';								
-			if upcomingMoviesPages-moviepage>10 and upcomingMoviesPages-10+i>moviepage+i:
-				html = html + '<a href=\"upcomingMovies?moviepage='+str(upcomingMoviesPages-10+i)+'\">'+str(upcomingMoviesPages-10+i)+'</a>  ';				
-
 	html = html + '</tr></td>'	
 	html = html + '</table></body></div></html>';
 	return html.encode('utf-8');
